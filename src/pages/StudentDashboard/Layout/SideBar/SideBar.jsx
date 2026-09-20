9a2240b11d5ac9cb52608046b5de1c4be96e291d
Asmil AP <asmilambalaparambil@gmail.com>
2026-09-20 16:57:57 +0530

    Fix Sidebar CSS import path

--- src/pages/StudentDashboard/Layout/SideBar/SideBar.jsx
+++ src/pages/StudentDashboard/Layout/SideBar/SideBar.jsx
@@ -4,7 +4,7 @@ import { signOut } from "firebase/auth";
 
 import { auth } from "../../../../firebase/firebase";
 
-import "./Sidebar.css";
+import "./SideBar.css";
 
 function Sidebar({ isOpen }) {
 
